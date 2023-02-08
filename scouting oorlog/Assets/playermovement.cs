953566132567f09ab55544fec4b2140a5300fce0
DanderOvskii <89800@glr.nl>
2023-02-08 16:49:58 +0100

    add project

---
+++ scouting oorlog/Assets/playermovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public CharacterController2D controler;
+    public Animator animator;
+    public  Rigidbody2D rigidBody; 
+
+    public float runSpeed = 40f;
+    
+   public bool jump = false;
+    bool crouch = false;
+   
+   
+
+    float horizontalMove = 0f;
+    // Update is called once per frame
+    void Update(){
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("speed", Mathf.Abs (horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+           
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+        crouch = true;
+
+
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+        if (Input.GetKeyDown("s") ){
+             rigidBody.gravityScale = 10;
+        }
+
+    }
+
+    public void OnLanding () 
+    {
+        animator.SetBool("IsJumping", false);
+        rigidBody.gravityScale = 3;
+        jump = false;
+        
+    }
+
+    public void Oncrouchung (bool ischouching)
+
+    {
+        animator.SetBool("iscrouching",ischouching);
+
+    }
+
+
+
+    void FixedUpdate ()
+    {
+        controler.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        
+    }
+
+
+}
