ec1bb4c42dcfc206eafd0625483a14ca12fd46ba
DanderOvskii <89800@glr.nl>
2023-02-12 14:51:21 +0100

    boom functie

--- scouting oorlog/Assets/boom/BoomDamage.cs
+++ scouting oorlog/Assets/boom/BoomDamage.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class BoomDamage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask playerLayers;
+    public int attackDamageBoom = 200;
+
+
+
 
     // Update is called once per frame
-    void Update()
+
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Debug.Log("hit");
+        if (other.gameObject.tag == "player2")
+        {
+            Attack();
+        }
+    }
+
+
+
+    void Attack()
+    {
+        animator.SetTrigger("atack");
+        Collider2D[] hitEnemies2 = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayers);
+
+        foreach (Collider2D enemy2 in hitEnemies2)
+        {
+            enemy2.GetComponent<HP2>().Damage2(attackDamageBoom);
+        }
+    }
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
