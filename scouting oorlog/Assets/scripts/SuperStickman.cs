fcb170a8d7fc3262754bc8939c2bf7edd847d81b
Dander <89800@glr.nl>
2023-03-03 13:38:47 +0100

    pauze menu

---
+++ scouting oorlog/Assets/scripts/SuperStickman.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperStickman : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 2;
+    [SerializeField] float jumpForce = 0;
+
+    PauseSystem pauseSystem;
+    Rigidbody2D rb2;
+    Animator anm;
+
+    private void Awake()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        anm = GetComponent<Animator>();
+        pauseSystem = FindObjectOfType<PauseSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pauseSystem.GetIsPaused()) { return; }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            anm.SetTrigger("Super");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 newVelocity = rb2.velocity;
+        newVelocity.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        rb2.velocity = newVelocity;
+    }
+}
